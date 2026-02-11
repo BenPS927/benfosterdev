0dcd92d5e7a8a06df9ebd37bf12ffe3b8c1a371a
BenPS927 <hemstitch147@posteo.uk>
2026-02-11 12:05:59 +0000

    more mobile changes

--- app/components/pages/home-page.tsx
+++ app/components/pages/home-page.tsx
@@ -10,7 +10,7 @@ import { useState } from 'react';
 export default function HomePage() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className="mx-auto ml-2 mr-2 mt-4 bg-gray-20">
+    <div className="mx-auto ml-0 lg:ml-2 mr-0 lg:mr-2 mt-2 lg:mt-4 bg-gray-20">
       <header className="sticky top-0 z-10 bg-gray-50 flex justify-between items-center rounded-t-md">
         <div className="p-4 ml-6">signature</div>
         <MenuIcon 
@@ -33,33 +33,33 @@ export default function HomePage() {
           <a href="#" className="block p-2 hover:bg-gray-100">Contact</a>
         </div>
 )}
-      <main className="flex flex-col lg:flex-row p-4 gap-4 rounded-b-md">
-        <div className="p-4 flex-1 min-w-0">
-          <h1 className="p-4 text-4xl md:text-6xl lg:text-8xl font-bebas mt-16 font-bold tracking-wide drop-shadow-md">IT Professional</h1>
-          <p className="p-4 mt-4 text-xl text-stone-500 font-oswald drop-shadow-md">Hi, I'm Ben, a passionate IT professional based in Bristol, England.<br />
+      <main className="flex flex-col lg:flex-row p-0 lg:p-4 gap-4 rounded-b-md">
+        <div className="p-0 lg:p-4 flex-1 min-w-0">
+          <h1 className="px-4 lg:p-4 text-4xl md:text-6xl lg:text-8xl font-bebas mt-0 lg:mt-16 font-bold tracking-wide drop-shadow-md">IT Professional</h1>
+          <p className="p-2 lg:p-4 mt-2 lg:mt-4 text-lg lg:text-xl text-stone-500 font-oswald drop-shadow-md">Hi, I'm Ben, a passionate IT professional based in Bristol, England.<br />
            Managing IT systems by day, building projects by night. </p>
 
-          <div className="flex p-4 mt-28 gap-8 items-center">
+          <div className="flex flex-wrap p-4 mt-8 lg:mt-28 gap-4 lg:gap-8 items-center">
             <p className="p-4 mt-4 text-stone-500 font-oswald font-bold">Passionate about <br /> tech</p>
             <div className="h-24 border-l-2 border-gray-300"></div>
-            <div className="w-24 h-24 flex-shrink-0 rounded-full bg-white shadow-lg border-1 border-gray-200 flex items-center justify-center hover:scale-110 cursor-pointer transition">
+            <div className="w-16 h-16 lg:w-24 lg:h-24 flex-shrink-0 rounded-full bg-white shadow-lg border-1 border-gray-200 flex items-center
+                 justify-center hover:scale-110 cursor-pointer transition">
               <LinkedInIcon sx={{ fontSize: 60, color: '#0A66C2', '&:hover': { color: '#004182' }, cursor: 'pointer', transition: 'color 0.3s' }} />
             </div>
-            <div className="w-24 h-24 flex-shrink-0 rounded-full bg-white shadow-lg border-1 border-gray-200 flex items-center justify-center hover:scale-110 cursor-pointer transition">
+            <div className="w-16 h-16 lg:w-24 lg:h-24 flex-shrink-0 rounded-full bg-white shadow-lg border-1 border-gray-200 flex items-center
+                 justify-center hover:scale-110 cursor-pointer transition">
               <GitHubIcon sx={{ fontSize: 60, color: '#181717', '&:hover': { color: '#e5e0ed' }, cursor: 'pointer', transition: 'color 0.3s' }} />
             </div>
-            <div className="w-24 h-24 flex-shrink-0 rounded-full bg-white shadow-lg border-1 border-gray-200"></div>
-           <div className="w-24 h-24 flex-shrink-0 rounded-full bg-white shadow-lg border-1 border-gray-200"></div>
           </div>
         </div>
-        <div className="flex-shrink-0 w-fit p-8 mr-8">
+        <div className="order-first lg:order-last flex-shrink-0 w-fit p-8 mr-8">
             <Box 
               component="img"
               src="/profilephoto2.png"
               alt="Ben Foster"
               sx={{ 
-                width: { xs: '80vw', lg: 500 }, 
-                height: { xs: '80vw', lg: 500 }, 
+                width: { xs: '50vw', lg: 500 }, 
+                height: { xs: '50vw', lg: 500 }, 
                 borderRadius: '50%', 
                 borderColor: 'white' 
               }}/>
