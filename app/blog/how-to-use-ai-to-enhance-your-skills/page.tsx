93f74a44a94e3e41c717af34531020f6c8be421c
BenPS927 <hemstitch147@posteo.uk>
2026-03-05 11:12:37 +0000

    Added GA tags to layout, and schema and meta tags to blog article

--- app/blog/how-to-use-ai-to-enhance-your-skills/page.tsx
+++ app/blog/how-to-use-ai-to-enhance-your-skills/page.tsx
@@ -1,10 +1,31 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Link from 'next/link'
 import AIEnhance from "@/app/components/blogs/how-to-use-ai-to-enhance-your-skills";
+import type { Metadata } from "next";
+import JsonLd from "@/app/components/shared/jsonld"
+
+export const metadata: Metadata = {
+  title: "How to Use AI to Enhance Your Skills | Ben Foster's Blog",
+  description: "Learn how to leverage AI tools to improve your skills and productivity in various domains.",
+}
+
+const schema = {
+  "@context": "https://schema.org",
+  "@type": "Blog",
+  "name": "Ben Foster | Blog",
+  "url": "https://benfosterdev.com/blog",
+  "description": "Thoughts and insights on React development, AI chatbots, and the journey to freelance.",
+  "author": {
+    "@type": "Person",
+    "name": "Ben Foster",
+    "url": "https://benfosterdev.com"
+  }
+}
 
 export default function Aienhance () {
     return (
         <div className="relative p-4">
+        <JsonLd schema={schema} />
            <div className="w-full flex flex-col lg:flex-row gap-0 lg:gap-24 p-2 lg:p-4">
                 <div className="fixed top-0 left-0 w-full h-full pointer-events-none z-0">
                     <img 
