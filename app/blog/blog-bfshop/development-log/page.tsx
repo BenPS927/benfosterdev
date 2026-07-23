4f95965b8a11e14246a035ba9bce00ad67a33a5e
Ben Foster <hemstitch147@posteo.uk>
2026-07-23 19:13:34 +1000

    did fix for dev log

--- app/blog/blog-bfshop/development-log/page.tsx
+++ app/blog/blog-bfshop/development-log/page.tsx
@@ -3,6 +3,8 @@ import { prisma } from '@/app/lib/prisma';
 import JsonLd from '@/app/components/shared/jsonld';
 import type { Metadata } from 'next';
 
+export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = {
     title: 'Development Log | BFshop Blog',
     description: 'Ongoing BFshop development updates of BFshop.',
