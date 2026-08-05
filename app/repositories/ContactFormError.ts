357a98815985c5d3e49f5865e92fee132a97a290
Ben Foster <hemstitch147@posteo.uk>
2026-08-05 20:44:07 +1000

    wrgfwf

--- app/repositories/ContactFormError.ts
+++ app/repositories/ContactFormError.ts
@@ -1,12 +1,17 @@
 import { prisma } from "@/app/lib/prisma";
 
 export async function ContactFormError(message: string) {
-  await prisma.contact_form_debug.create({
-    data: {
-      webhook: "Success",
-      receive_email: "Success",
-      send_email: "Success",
-      notes: message,
-    },
-  });
+    const data = {
+        webhook: "Failed",
+        receive_email: "N/A",
+        send_email: "N/A",
+        notes: message,
+    };
+
+    console.log(data);
+
+    const result = await prisma.contact_form_debug.create({
+        data,
+    }); 
+    console.log(result)
 }
